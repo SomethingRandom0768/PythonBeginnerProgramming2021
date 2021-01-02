bbb35347f71608f1fed4d1c95b8d9bd634a0d556
SomethingRandom <shishkebab259@gmail.com>
2021-01-02 13:13:20 -0600

    Adding a new exercise

--- Chapter 2/Exercises/famous_quote.py
+++ Chapter 2/Exercises/famous_quote.py
@@ -1,6 +1,1 @@
-name = "Cal Newport"
-quote = "If you want to love what you do, do what Steve Jobs did and not what he said."
-
-message = f'{name} once said, "{quote}"'
-
-print(message)
+print('Cal Newport once said, "If you want to love what you do, do what Steve Jobs did and not what he said." ')
