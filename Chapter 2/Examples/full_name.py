f30af3daf01a48ab7e89c51bafbc6152de1334a1
SomethingRandom <shishkebab259@gmail.com>
2021-01-02 13:23:22 -0600

    Updating the file

--- Chapter 2/Examples/full_name.py
+++ Chapter 2/Examples/full_name.py
@@ -1,3 +1,5 @@
+# Remember, they're called f-STRINGS, should help with what they should look like.
+
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{ first_name } { last_name }"
---
+++ Chapter 2/Exercises/stripping_names.py
@@ -0,0 +1,7 @@
+name = "\tMonkey\t"
+
+print(name)
+
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
