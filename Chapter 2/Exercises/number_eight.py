8d9feb835165e483ea2a4c58b65edd1bb6984cbd
SomethingRandom <shishkebab259@gmail.com>
2021-01-03 12:51:34 -0600

    Updating a file

--- Chapter 2/Exercises/number_eight.py
+++ Chapter 2/Exercises/number_eight.py
@@ -1,3 +1,5 @@
+# Using multiple operators so that the answer to each of these expressions is 8
+
 print(5 + 3) # addition operator
 print(11 - 3) # subtraction operator
 print(4 * 2) # multiplication operator
