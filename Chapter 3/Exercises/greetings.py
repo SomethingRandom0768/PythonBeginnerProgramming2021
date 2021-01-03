9baec6664740431fe3125650315c959151040aab
SomethingRandom <shishkebab259@gmail.com>
2021-01-03 14:37:05 -0600

    Adding files

---
+++ Chapter 3/Exercises/greetings.py
@@ -0,0 +1,12 @@
+friend_list = ['monkey', 'SomethingRandom', 'Kitty', 'SomethingDeliberate']
+
+message1 = f"Hi {friend_list[0]}! You're my friend! "
+message2 = f"Hi {friend_list[1]}! You're my friend! "
+message3 = f"Hi {friend_list[2]}! You're my friend! "
+message4 = f"Hi {friend_list[3]}! You're my friend! "
+
+print(message1)
+print(message2)
+print(message3)
+print(message4)
+
---
+++ Chapter 3/Exercises/my_list.py
@@ -0,0 +1,13 @@
+# Yes, I like these games. Pretty cool for being free games.
+fangames = ["Five Nights At Candy's", "POPGOES", "The Joy Of Creation", "One Night at Flumpty's"]
+
+message1 = f"I want to play {fangames[0]}."
+message2 = f"I want to play {fangames[1]}."
+message3 = f"I want to play {fangames[2]}."
+message4 = f"I want to play {fangames[3]}."
+
+
+print(message1)
+print(message2)
+print(message3)
+print(message4)
