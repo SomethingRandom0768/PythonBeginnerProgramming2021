6a052db9158698d6b7d694bf40ac6b1e2f61aad0
SomethingRandom <shishkebab259@gmail.com>
2021-01-05 00:16:51 -0600

    Adding file

---
+++ Chapter 3/Exercises/3-4guest_list.py
@@ -0,0 +1,10 @@
+guest_list = ["shiskebab", "SomethingDeliberate", "SomethingRandom"]
+# I usually avoid using real life references when it comes to the programs
+
+message1 = f"Hi {guest_list[0]}, would you like to come for dinner tonight?"
+message2 = f"Hi {guest_list[1]}, would you like to come for dinner tonight?"
+message3 = f"Hi {guest_list[2]}, would you like to come for dinner tonight?"
+
+print(message1) # Printing each message out separately
+print(message2)
+print(message3)
