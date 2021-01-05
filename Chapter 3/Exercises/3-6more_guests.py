e0e3cd1f82b994e35eac8e052c0f1cb99b86a7da
SomethingRandom <shishkebab259@gmail.com>
2021-01-05 13:26:43 -0600

    Adding a file

---
+++ Chapter 3/Exercises/3-6more_guests.py
@@ -0,0 +1,22 @@
+guest_list = ["shishkebab", "SomethingDeliberate", "SomethingRandom"]
+# Using the same list from 3-4
+
+print("It turns out that we've found a bigger table so we can invite more people!")
+
+guest_list.insert(0, "Kitty")
+guest_list.insert(1, "Oh Deer")
+guest_list.append("Pusheen")
+
+message1 = f"Hi {guest_list[0]}, would you like to come for dinner tonight?"
+message2 = f"Hi {guest_list[1]}, would you like to come for dinner tonight?"
+message3 = f"Hi {guest_list[2]}, would you like to come for dinner tonight?"
+message4 = f"Hi {guest_list[3]}, would you like to come for dinner tonight?"
+message5 = f"Hi {guest_list[4]}, would you like to come for dinner tonight?"
+message6 = f"Hi {guest_list[5]}, would you like to come for dinner tonight?"
+
+print(message1) # Pretty much a copy and paste
+print(message2)
+print(message3)
+print(message4)
+print(message5)
+print(message6)
