cf45330af3add0c0c63674036ac235bace592381
SomethingRandom <shishkebab259@gmail.com>
2021-01-05 20:40:01 -0600

    Adding a file

---
+++ Chapter 3/Exercises/3-10every-function.py
@@ -0,0 +1,15 @@
+list_of_things_excited = ['Yamishibai Season 8', 'Disenchantment Part 3', 'Halo Infinite', "Five Nights At Freddy's: Security Breach"]
+
+list_of_things_excited.reverse() # Regardless if you reverse the order, I still am excited for them all
+
+list_of_things_excited.reverse()  # Reversing the list back to its original state
+
+number_amount = len(list_of_things_excited) # using the len function
+
+amount = f"There are {number_amount} things I am excited for this year!"
+
+print( sorted( list_of_things_excited, reverse = True ) ) # I like to space out my parenthesis, makes it easier to read
+
+list_of_things_excited.sort(reverse= True)
+
+print(list_of_things_excited) # Should be just as the way it was originally
