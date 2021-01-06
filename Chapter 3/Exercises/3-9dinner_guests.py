51ead7ca6e6520c32ded52ee8e65e6c676446fdd
SomethingRandom <shishkebab259@gmail.com>
2021-01-05 20:25:17 -0600

    Adding a file

--- Chapter 3/Exercises/3-9dinner_guests.py
+++ Chapter 3/Exercises/3-9dinner_guests.py
@@ -1,3 +1,8 @@
 guest_list = ["shishkebab", "SomethingDeliberate", "SomethingRandom"]
 # We're using the original list from 3-4
 
+list_amount = len(guest_list)
+
+answer = f"We're going to be inviting {list_amount} people to this dinner :D"
+
+print(answer)
