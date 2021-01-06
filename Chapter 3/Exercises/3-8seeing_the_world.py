be7b6d13fa964f75fc91a29c4433e2dc452777dd
SomethingRandom <shishkebab259@gmail.com>
2021-01-05 20:21:38 -0600

    Adding a file

---
+++ Chapter 3/Exercises/3-8seeing_the_world.py
@@ -0,0 +1,20 @@
+places = ['Brazil', 'England', 'Australia', 'Disneyland', 'Kitten Land'] # please let Kitten Land be a place.
+
+print(places)
+
+print( sorted(places) )
+
+print(places) # Printing out the original list
+
+print( sorted(places, reverse=True) ) # printing out a reverse alphabetical version of the list
+
+print(places) # Still printing out original list
+
+print( places.reverse() ) # printing out the reverse of the list
+
+print( places.reverse() ) # reversing the list again to show that it's back to its original order
+
+print( places.sort() ) # printing in alphabetical order
+
+print( places.sort(reverse=True) ) # printing in reverse alphabetical order
+
