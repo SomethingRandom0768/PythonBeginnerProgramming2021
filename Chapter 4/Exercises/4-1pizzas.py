993e4c3403a2fdfca62c3ab2adbab30c1fe282a1
SomethingRandom <shishkebab259@gmail.com>
2021-01-09 12:49:15 -0600

    Adding files

---
+++ Chapter 4/Exercises/4-1pizzas.py
@@ -0,0 +1,6 @@
+pizzatypes = ['cheese', 'buffalo chicken', 'chicken']
+
+for pizza in pizzatypes:
+    print(f" I like { pizza.title() } pizza")
+
+print("\nI really like pizza!")
---
+++ Chapter 4/Exercises/4-2animals.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'ferret']
+
+for animal in animals:
+    print(f"A {animal} would be a nice pet to have")
+
+print("All of these pets would be good to have!")
