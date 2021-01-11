7525fbdb1ef3cc0e868e3879fac6863c3d989794
SomethingRandom <shishkebab259@gmail.com>
2021-01-10 18:34:20 -0600

    Tiny fix

--- Chapter 4/Exercises/4-1pizzas.py
+++ Chapter 4/Exercises/4-1pizzas.py
@@ -1,6 +1,6 @@
-pizzatypes = ['cheese', 'buffalo chicken', 'chicken']
+pizza_types = ['cheese', 'buffalo chicken', 'chicken']
 
-for pizza in pizzatypes:
+for pizza in pizza_types:
     print(f" I like { pizza.title() } pizza")
 
 print("\nI really like pizza!")
