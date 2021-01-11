83935f55481b76ecd06847198f0c95c9cb2ebcf0
SomethingRandom <shishkebab259@gmail.com>
2021-01-10 21:53:37 -0600

    Tiny fix

---
+++ Chapter 4/Examples/even_numbers.py
@@ -0,0 +1,3 @@
+# Starts at 2, keeps going up by 2 and until it passes 11
+even_numbers = list(range (2, 11, 2) )
+print(even_numbers)
---
+++ Chapter 4/Examples/first_numbers.py
@@ -0,0 +1,3 @@
+# Starts at 1 and then ends at 5 due to off-by-one behavior
+for value in range(1, 6):
+    print(value)
